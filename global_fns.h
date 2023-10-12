6de3ca625b997003694aabae57f96de8c9ff7258
David Dolman <david@dawdolman.com>
2023-10-12 01:11:30 +0100

    Cleaned up global_fns.h Moved t_reg_args to EDL.

--- global_fns.h
+++ global_fns.h
@@ -3,34 +3,12 @@
 // File		globals_fns.h
 // Date:	26 Sept 2000
 
-class scoreboard;  // Units are declared here to allow forward references
-class control_box;    // between them. HASE automatically declares them from
-class clock;       // the dlx.edl file, but in sequence, only allowing
-class registers;   // backward references.
-class memory;
-class instr_fetch;
-class instr_decode;
-class int_exe;
-class flt_exe;
-class mul_exe;
-class div_exe;
-class execute;
-class mem_access;
-class write_back;
-
-
 int pow(int, int);  // forms exponential without using doubles
 
 float floatFromInt(int v);
 int floatToInt(float d);
 
-struct t_reg_args {
-  char type;       // Either R or F
-  int  number;     // Register number
-  int  index;	   // Offset (if any)
-};
-
-class t_reg_args Decode_Register(char *Instruction);
+t_reg_args Decode_Register(char *Instruction);
 
 int Decode_LOAD(t_dlx_instrn_set Instruction);
 
